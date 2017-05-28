206ce0f6a7deca6b8279ea776efb5d936fd6e02e
korablique <yulia-11a@yandex.ru>
2017-05-28 21:22:46 +0700

    Кнопка Сохранить в ShowResultDialogFragment неактивна, пока не введено название

--- app/src/main/java/korablique/recipecalculator/ShowResultDialogFragment.java
+++ app/src/main/java/korablique/recipecalculator/ShowResultDialogFragment.java
@@ -8,6 +8,9 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
@@ -25,6 +28,8 @@ import static korablique.recipecalculator.FoodstuffsContract.Foodstuffs.COLUMN_N
 import static korablique.recipecalculator.FoodstuffsContract.Foodstuffs.TABLE_NAME;
 
 public class ShowResultDialogFragment extends DialogFragment {
+    private EditText recipeName;
+
     @Override
     @NonNull public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
@@ -51,7 +56,7 @@ public class ShowResultDialogFragment extends DialogFragment {
         LinearLayout layout = new LinearLayout(getContext());
         layout.setOrientation(LinearLayout.VERTICAL);
         layout.setLayoutParams(params);
-        final EditText recipeName = new EditText(getContext());
+        recipeName = new EditText(getContext());
         recipeName.setHint(R.string.recipe_name);
         layout.addView(recipeName);
         TextView result = new TextView(getContext());
@@ -59,11 +64,11 @@ public class ShowResultDialogFragment extends DialogFragment {
         layout.addView(result);
 
         builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                @Override
                 public void onClick(DialogInterface dialog, int id) {
-                    ShowResultDialogFragment.this.getDialog().cancel(); //TODO: или надо dialog из аргументов использовать?
+                    dialog.cancel();
                 }
-            })
-            .setNeutralButton(R.string.save, new DialogInterface.OnClickListener() {
+            }).setNeutralButton(R.string.save, new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     ContentValues values = new ContentValues();
@@ -80,6 +85,28 @@ public class ShowResultDialogFragment extends DialogFragment {
                 }
             });
         builder.setView(layout);
-        return builder.create();
+        final AlertDialog dialog = builder.create();
+        dialog.show();
+        dialog.getButton(AlertDialog.BUTTON_NEUTRAL).setEnabled(false);
+
+        recipeName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (TextUtils.isEmpty(s)) {
+                    dialog.getButton(AlertDialog.BUTTON_NEUTRAL).setEnabled(false);
+                } else {
+                    dialog.getButton(AlertDialog.BUTTON_NEUTRAL).setEnabled(true);
+                }
+            }
+        });
+        return dialog;
     }
 }
