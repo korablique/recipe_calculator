41dfb5a4755bd9059c5da34e84fca3cb6fb9798a
korablique <yulia-11a@yandex.ru>
2019-05-23 14:23:45 +0300

    Правки по ревью

--- app/src/main/java/korablique/recipecalculator/database/room/HistoryDao.kt
+++ app/src/main/java/korablique/recipecalculator/database/room/HistoryDao.kt
@@ -4,10 +4,10 @@ import android.database.Cursor
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
-import korablique.recipecalculator.database.FoodstuffsContract
 import korablique.recipecalculator.database.FoodstuffsContract.*
 import korablique.recipecalculator.database.HistoryContract.*
 import korablique.recipecalculator.database.HistoryContract.ID
+import korablique.recipecalculator.database.FoodstuffsContract.ID as FOODSTUFF_ID
 
 @Dao
 interface HistoryDao {
@@ -29,11 +29,11 @@ interface HistoryDao {
             " ORDER BY $COLUMN_NAME_DATE ASC")
     fun loadFoodstuffsIdsForPeriod(from: Long, to: Long): List<Long>
 
-    @Query("SELECT $FOODSTUFFS_TABLE_NAME.$ID, $COLUMN_NAME_FOODSTUFF_NAME, $COLUMN_NAME_PROTEIN," +
-            " $COLUMN_NAME_FATS, $COLUMN_NAME_CARBS, $COLUMN_NAME_CALORIES" +
+    @Query("SELECT $FOODSTUFFS_TABLE_NAME.$FOODSTUFF_ID, $COLUMN_NAME_FOODSTUFF_NAME," +
+            " $COLUMN_NAME_PROTEIN, $COLUMN_NAME_FATS, $COLUMN_NAME_CARBS, $COLUMN_NAME_CALORIES" +
             " FROM $HISTORY_TABLE_NAME LEFT OUTER JOIN $FOODSTUFFS_TABLE_NAME" +
             " ON $HISTORY_TABLE_NAME.$COLUMN_NAME_FOODSTUFF_ID" +
-            "=$FOODSTUFFS_TABLE_NAME.${FoodstuffsContract.ID}" +
+            "=$FOODSTUFFS_TABLE_NAME.$FOODSTUFF_ID" +
             " WHERE $COLUMN_NAME_DATE >= :from AND $COLUMN_NAME_DATE <= :to" +
             " AND $COLUMN_NAME_IS_LISTED = 1" +
             " ORDER BY $COLUMN_NAME_DATE ASC")
@@ -41,19 +41,19 @@ interface HistoryDao {
 
     @Query("SELECT * FROM $HISTORY_TABLE_NAME LEFT OUTER JOIN $FOODSTUFFS_TABLE_NAME" +
             " ON $HISTORY_TABLE_NAME.$COLUMN_NAME_FOODSTUFF_ID" +
-            "=${FOODSTUFFS_TABLE_NAME}.${FoodstuffsContract.ID}" +
+            "=$FOODSTUFFS_TABLE_NAME.$FOODSTUFF_ID" +
             " WHERE $COLUMN_NAME_DATE >= :from AND $COLUMN_NAME_DATE <= :to" +
             " ORDER BY $COLUMN_NAME_DATE ASC")
     fun loadHistoryForPeriod(from: Long, to: Long): Cursor
 
     @Query("SELECT * FROM $HISTORY_TABLE_NAME LEFT OUTER JOIN $FOODSTUFFS_TABLE_NAME" +
-            " ON ${HISTORY_TABLE_NAME}.${COLUMN_NAME_FOODSTUFF_ID}" +
-            "=${FOODSTUFFS_TABLE_NAME}.${FoodstuffsContract.ID} ORDER BY $COLUMN_NAME_DATE DESC")
+            " ON $HISTORY_TABLE_NAME.$COLUMN_NAME_FOODSTUFF_ID" +
+            "=$FOODSTUFFS_TABLE_NAME.$FOODSTUFF_ID ORDER BY $COLUMN_NAME_DATE DESC")
     fun loadHistory(): Cursor
 
     @Query("SELECT * FROM $HISTORY_TABLE_NAME LEFT OUTER JOIN " + FOODSTUFFS_TABLE_NAME +
-            " ON ${HISTORY_TABLE_NAME}.${COLUMN_NAME_FOODSTUFF_ID}" +
-            "=${FOODSTUFFS_TABLE_NAME}.${FoodstuffsContract.ID} ORDER BY $COLUMN_NAME_DATE DESC LIMIT :limit")
+            " ON $HISTORY_TABLE_NAME.$COLUMN_NAME_FOODSTUFF_ID" +
+            "=$FOODSTUFFS_TABLE_NAME.$FOODSTUFF_ID ORDER BY $COLUMN_NAME_DATE DESC LIMIT :limit")
     fun loadHistoryWithLimit(limit: Int): Cursor
 }
 
