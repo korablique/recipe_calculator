f5dc10eea5329fd26cf5c6754a9813b51e40ff86
Danil <blazer007@yandex.ru>
2019-10-17 21:25:47 +0300

    Добавлен поиск надписей на латинице кирилицой (https://trello.com/c/vlvmhtir)

--- app/src/main/java/korablique/recipecalculator/util/FuzzySearcher.java
+++ app/src/main/java/korablique/recipecalculator/util/FuzzySearcher.java
@@ -30,34 +30,44 @@ public class FuzzySearcher {
     /**
      * Performs fuzzy search, returns items which it considered to be similar to the search
      * query. Returned items are ordered from the most similar to the least similar.
-     * @param query search query
+     * @param inputQuery search query
      * @param items items among which search will be done
      * @param toStringFunction a functional object to turn items into strings
      * @param limit limit of items in result
      * @param <T> the type of items - can be any type
      * @return similar to the search query items, ordered from the most similar to the least
      */
-    public static <T> List<T> search(String query,
+    public static <T> List<T> search(String inputQuery,
                                      Collection<T> items,
                                      Function1arg<String, T> toStringFunction,
                                      int limit) {
+        String transliteratedQuery = RussianEnglishTransliteratorKt.transliterateRussian(inputQuery);
+        String[] queries;
+        if (transliteratedQuery.equals(inputQuery)) {
+            queries = new String[] { inputQuery };
+        } else {
+            queries = new String[] { inputQuery, transliteratedQuery };
+        }
         List<Pair<Integer, T>> resultWithRatio = new ArrayList<>();
         for (T item : items) {
             String itemString = toStringFunction.call(item);
             // The 'weightedRatio' function uses several fuzzy search
             // algorithms - it combines their output for better results.
-            int weightedRatio = FuzzySearch.weightedRatio(query, itemString);
-            // The 'partialRatio' function gives best results for comparing strings of very
-            // different size. For example, if you'd compare strings 'product' and
-            // 'very long string with product in the middle and other words on the sides', then
-            // the 'partialRatio' function will return 100, because first string is a substring of
-            // the second one.
-            int partialRatio = FuzzySearch.partialRatio(query, itemString);
-            // Partial and weighted ratio are both trusted algorithms, but they work best in
-            // different situations - that's why we use both.
-            if (partialRatio >= SEARCH_RATIO_THRESHOLD
-                    || weightedRatio >= SEARCH_RATIO_THRESHOLD) {
-                resultWithRatio.add(Pair.create((partialRatio+weightedRatio)/2, item));
+            for (String query : queries) {
+                int weightedRatio = FuzzySearch.weightedRatio(query, itemString);
+                // The 'partialRatio' function gives best results for comparing strings of very
+                // different size. For example, if you'd compare strings 'product' and
+                // 'very long string with product in the middle and other words on the sides', then
+                // the 'partialRatio' function will return 100, because first string is a substring of
+                // the second one.
+                int partialRatio = FuzzySearch.partialRatio(query, itemString);
+                // Partial and weighted ratio are both trusted algorithms, but they work best in
+                // different situations - that's why we use both.
+                if (partialRatio >= SEARCH_RATIO_THRESHOLD
+                        || weightedRatio >= SEARCH_RATIO_THRESHOLD) {
+                    resultWithRatio.add(Pair.create((partialRatio + weightedRatio) / 2, item));
+                    break;
+                }
             }
         }
 
