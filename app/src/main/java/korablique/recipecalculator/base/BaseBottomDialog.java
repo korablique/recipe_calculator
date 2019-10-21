f9b018ab4e5b15c97a75d7109de07aa7986765dd
Danil <blazer007@yandex.ru>
2019-10-21 11:18:33 +0300

    Поперемещал паддинги и маржины диалогов так, чтобы в низ диалогов можно было вставить вьюху клавиатуры, которая визуально не будет частью диалога (https://trello.com/c/cEzTQVMd)

--- app/src/main/java/korablique/recipecalculator/base/BaseBottomDialog.java
+++ app/src/main/java/korablique/recipecalculator/base/BaseBottomDialog.java
@@ -23,7 +23,7 @@ public class BaseBottomDialog extends DialogFragment {
             layoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
             layoutParams.gravity = Gravity.BOTTOM;
             dialog1.getWindow().setAttributes(layoutParams);
-            dialog1.getWindow().setBackgroundDrawable(getContext().getResources().getDrawable(R.drawable.new_card_background));
+            dialog1.getWindow().setBackgroundDrawable(getContext().getResources().getDrawable(R.drawable.bottom_dialog_transparent_background));
         });
         dialog1.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
         return dialog1;
