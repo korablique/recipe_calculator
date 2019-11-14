f4ddd59b237cdfbe1984e8e9fac96f6c267532d9
Danil <blazer007@yandex.ru>
2019-11-14 21:27:33 +0300

    Допускаем только 1 цифру после точки у всех CalcEditText (https://trello.com/c/ExE4FytY)

--- app/src/main/java/korablique/recipecalculator/ui/calckeyboard/CalcEditText.kt
+++ app/src/main/java/korablique/recipecalculator/ui/calckeyboard/CalcEditText.kt
@@ -1,16 +1,21 @@
 package korablique.recipecalculator.ui.calckeyboard
 
 import android.content.Context
+import android.text.InputFilter
+import android.text.SpannableString
 import android.util.AttributeSet
 import android.widget.EditText
 import com.udojava.evalex.Expression
 import korablique.recipecalculator.R
+import korablique.recipecalculator.ui.inputfilters.DecimalNumberInputFilter
 import korablique.recipecalculator.ui.inputfilters.FunctionalInputFilter
 import java.math.BigDecimal
 import java.util.regex.Pattern
 
 open class CalcEditText : EditText {
     private val extraOperatorsRegex = Pattern.compile(""".*[\-+*/][\-+*/].*""")
+    private val operatorsRegex = Pattern.compile("""[\-+×÷]""")
+    private val numbersFilters = mutableListOf<InputFilter>()
     private var minValue: BigDecimal
     private var maxValue: BigDecimal
 
@@ -24,20 +29,29 @@ open class CalcEditText : EditText {
                 attrs,
                 R.styleable.CalcEditText,
                 0, 0)
+        val digitsAfterDecimalDot: Int
         try {
             minValue = BigDecimal(typedArray.getFloat(R.styleable.CalcEditText_min_value, Float.MIN_VALUE).toDouble())
             maxValue = BigDecimal(typedArray.getFloat(R.styleable.CalcEditText_max_value, Float.MAX_VALUE).toDouble())
+            digitsAfterDecimalDot = typedArray.getInt(R.styleable.CalcEditText_digits_after_decimal_dot, Int.MAX_VALUE)
         } finally {
             typedArray.recycle()
         }
 
         // Зададим фильтр, не допускающий строки, которые невозможно вычислить
         val inputFilter = FunctionalInputFilter.ofFunction(this::isAcceptableUserInput)
-        setFilters(arrayOf(inputFilter))
+        setFilters(getFilters() + arrayOf(inputFilter))
+
+        if (digitsAfterDecimalDot != Int.MAX_VALUE) {
+            numbersFilters += DecimalNumberInputFilter.ofNDigitsAfterPoint(digitsAfterDecimalDot)
+        }
     }
 
     private fun isAcceptableUserInput(str: String): Boolean {
         val value = calcValueAsBigDecimal(str) ?: return false
+        if (!isAllowedByNumbersFilters(str)) {
+            return false
+        }
         return isAcceptableBigDecimal(value)
     }
 
@@ -71,6 +85,34 @@ open class CalcEditText : EditText {
         } catch (e: Expression.ExpressionException) {
             return null
         }
+    }
+
+    private fun isAllowedByNumbersFilters(inputStr: String): Boolean {
+        // Раздробим входную строку на подстроки, содержащие только числа,
+        // прогоним каждое из этих строк-чисел через numbersFilters.
+        // Мотивация - InputFilter'ы в EditText'ах фильтруют всю вводимую пользователем строку целиком,
+        // не пытаясь её дробить на части, но нам нужно уметь фильтровать не всю строку целиком,
+        // а числа в ней.
+        numbersFilters.forEach { numberFilter ->
+            // Разделим строку вида "2+11-4" на строки ["2", "11", "4"]
+            val numbersStrs = inputStr.split(operatorsRegex)
+            numbersStrs.forEach { numberStr ->
+                // mockDest - фальшивое destination для вставки отфильтрованной строки в него,
+                // нужно для того, чтобы filter думал, что numberStr куда-то вставляется и
+                // отфильтровал бы его хорошенько.
+                val mockDest = SpannableString("")
+                val filterResult = numberFilter.filter(
+                        numberStr, 0, numberStr.length,
+                        mockDest, 0, 0)
+                // Если numberFilter не вернул null, то ему чем-то не нравится входящая строка.
+                // Если входящая строка фильтру чем-то не нравится,
+                // то будем считать inputStr неприемлимым.
+                if (filterResult != null) {
+                    return false
+                }
+            }
+        }
+        return true
     }
 
     private fun isAcceptableBigDecimal(value: BigDecimal): Boolean {
